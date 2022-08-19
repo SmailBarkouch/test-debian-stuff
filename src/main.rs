8c94ee334efdb77431ce891e3a89ffa1f251976f
Smail Barkouch <smailbarkouch1@gmail.com>
2022-08-19 14:23:23 -0400

    verified uploading, getting latest release, and getting release by tag works

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs::File;
 use github_release::{AuthenticatedUser, RepoInfo};
 
 fn main() {
@@ -8,7 +9,12 @@ fn main() {
         repo_name: "test-debian-stuff".to_string()
     };
 
-    auth_user.get_latest_release(&repo_info).unwrap();
+    println!("LATEST FIELD: {}", auth_user.get_release_by_tag_name(&repo_info, "0.1").unwrap().id);
+    
+//     74857070
+    
+    // let info = auth_user.upload_release_asset(&repo_info, 74857070, "android.zip", "application/zip", File::open("/home/smailbarkouch/Downloads/android.zip").unwrap(), None).unwrap();
 
-    println!("Success!");
+    // println!("THE INFO: {:?}", info);
+
 }
